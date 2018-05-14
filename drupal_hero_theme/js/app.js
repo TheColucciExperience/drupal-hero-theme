04969141a6f6ae4a77f6d05c18b5818dfa4dc1af
Bruno Colucci <bruno.colucci@jussi.com.br>
2018-05-14 14:05:36 -0300

    Navbar button complete

--- drupal_hero_theme/js/app.js
+++ drupal_hero_theme/js/app.js
@@ -7,7 +7,66 @@
 
 	$( function initScript() {
 
-		console.log( 'ok' );
+		// Block for navbar interactions
+
+		{
+
+			// Object to hold state of navbar content
+
+			let navState = {
+				isShown: false,
+				userCanToggle: true
+			};			
+
+			// Adding click listener
+
+			$( '.js-navbar-button' ).on( 'click', function toggleContent() {
+
+				// Getting dom elements (with jQuery)
+
+				const $topBar = $( '.js-navbar-bar' ).first(),
+					$bottomBar = $( '.js-navbar-bar' ).last(),
+					$navbarContentContainer = $( '.js-navbar-content-container' );
+
+				// Only if user can toggle
+
+				if ( navState.userCanToggle ) {
+
+					navState.userCanToggle = !navState.userCanToggle;
+
+					// Bar animations for state changes
+
+					if ( !navState.isShown ) {  
+
+						// Bar animations
+
+						$topBar.css( 'animation', 'topBarStart .4s ease forwards' );
+						$bottomBar.css( 'animation', 'bottomBarStart .4s ease forwards' );
+
+					}
+					else {
+						$topBar.css( 'animation', 'topBarRestart .4s ease forwards' );
+						$bottomBar.css( 'animation', 'bottomBarRestart .4s ease forwards' );
+					}
+
+					// Toggling content container
+
+					$navbarContentContainer.toggle(
+						400,
+						// This will return user control and register state change
+						function changeStates() {
+							navState.isShown = !navState.isShown;
+							navState.userCanToggle = !navState.userCanToggle;
+						}
+					);
+
+				}
+
+			} );
+
+		}
+
+		
 
 	} );
 

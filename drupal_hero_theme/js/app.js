21e9455be8e5d02736744cca7c93aeb9eb6cb1c8
TheColucciExperience <thecolucciexperience@outlook.com>
2018-05-14 00:20:25 -0300

    General project setup ready

---
+++ drupal_hero_theme/js/app.js
@@ -0,0 +1,16 @@
+
+// IIFE to contain main script code and keep the global scope clean
+
+(function initLoad(window, document, $) {
+
+	// Executing code when page loads
+
+	$( function initScript() {
+
+		console.log( 'ok' );
+
+	} );
+
+
+
+})(window, document, jQuery);
